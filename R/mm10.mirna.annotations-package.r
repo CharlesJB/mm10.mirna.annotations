eb25f97f5211a1672f7811b6f61ec0262ca0020b
Charles Joly Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>
2015-04-28 17:20:37 -0400

    Added the miranda dataset
    Signed-off-by: Charles Joly Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>

--- R/mm10.mirna.annotations-package.r
+++ R/mm10.mirna.annotations-package.r
@@ -3,3 +3,25 @@
 #' @name mm10.mirna.annoations.
 #' @docType package
 NULL
+
+#' miRanda mm10
+#'
+#' This dataset is a \code{GRanges} object corresponding to the "Good mirSVR
+#' score, Conserved miRNA" version of the database downloaded from the "August
+#' 2010 Release Downloads" section:
+#' http://www.microrna.org/microrna/getDownloads.do
+#'
+#' The data in the \code{GRanges} object correspond to the predicted miRNA
+#' targets.
+#'
+#' The \code{GRanges} have to following metadata columns:
+#'   * mirna_name
+#'   * gene_symbol
+#'
+#' \itemize{
+#'   \item mirna_name Name of the miRNA for the current target.
+#'   \item gene_symbol Name of the targeted gene.
+#' }
+#'
+#' @name miranda
+NULL
